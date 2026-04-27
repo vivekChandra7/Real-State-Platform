1667b60af1219a7474f860251ff9af43ac166d85
Vivek <vivekchandra2462@gmail.com>
2026-04-27 23:57:37 +0530

    server.js

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+import http from "http";
+import { connectDB } from "./config/db.js";
+import authRouter from "./routes/auth.routes.js";
+import userRouter  from "./routes/user.routes.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//Db
+connectDB();
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Routes
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
