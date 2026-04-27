db447e968b6166d90d93e03c50912ef65905d7cf
Vivek <vivekchandra2462@gmail.com>
2026-04-28 00:10:09 +0530

    add Cloudinary upload utility using buffer streams

---
+++ backend/utils/uploadToCloudinary.js
@@ -0,0 +1,19 @@
+import cloudinary from "../config/cloudinary";
+import streamifier from "streamifier";
+
+export const uploadToCloudinary = (buffer, folder="general") => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      {
+        folder: folder,
+      },
+      (error, result) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(result);
+      },
+    );
+    streamifier.createReadStream(buffer).pipe(stream);
+  });
+};
