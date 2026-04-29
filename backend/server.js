e830382e593900793fbc014a3d39e20833bfedbd
Vivek <vivekchandra2462@gmail.com>
2026-04-29 23:21:46 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import http from "http";
 import { connectDB } from "./config/db.js";
 import authRouter from "./routes/auth.routes.js";
-import userRouter  from "./routes/user.routes.js";
+import userRouter from "./routes/user.routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -18,7 +18,7 @@ app.use(express.json());
 
 //Routes
 app.use("/api/auth", authRouter);
-app.use("/api/users", userRouter);
+app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
