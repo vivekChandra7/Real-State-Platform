f022846369c7d12e2d43a6b953cf4679ef6bdc93
Vivek <vivekchandra2462@gmail.com>
2026-04-29 23:23:40 +0530

    import cloudinary

--- backend/utils/uploadToCloudinary.js
+++ backend/utils/uploadToCloudinary.js
@@ -1,4 +1,4 @@
-import cloudinary from "../config/cloudinary";
+import cloudinary from "../config/cloudinary.js";
 import streamifier from "streamifier";
 
 export const uploadToCloudinary = (buffer, folder="general") => {
