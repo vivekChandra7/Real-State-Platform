6ca62a57077f7e1d3430c24cae499903af61bc83
Vivek <vivekchandra2462@gmail.com>
2026-04-29 23:22:55 +0530

    inquiry model

---
+++ backend/models/inquiry.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import Property from "./property.model";
+import User from "./user.model";
+
+const inquirySchema = new mongoose.Schema(
+  {
+    Property: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Property",
+      required: true,
+    },
+    buyer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    seller: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
+
+const Inquiry = mongoose.model("Inquiry", inquirySchema);
+export default Inquiry;
