258b4f4d8c712f2d839aabdb9caf3dd8c29203b6
Vivek <vivekchandra2462@gmail.com>
2026-05-01 23:59:26 +0530

    routes

---
+++ backend/routes/property.routes.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { 
+  getAllProperties, 
+  addProperty, 
+  getMyProperties, 
+  updateProperty, 
+  deleteProperty, 
+  updatePropertyStatus, 
+  getPropertyCounts, 
+  getPropertyDetails, 
+  getSellerDashboard 
+} from "../controllers/property.controller.js"; 
+import { protect, authorize } from "../middlewares/auth.middleware.js";
+
+
+const propertyRouter = express.Router();
+
+propertyRouter.get("/", getAllProperties);
+// protect the routes that only seller can do these works
+propertyRouter.post("/add", protect, authorizeRoles("seller"), upload.array("images", 10), addProperty);
+propertyRouter.get("/my", protect, authorize("seller"), getMyProperties);
+propertyRouter.put("/:id",protect, authorize("seller"), updateProperty);
+propertyRouter.delete("/:id", protect, authorize("seller"), deleteProperty);
+propertyRouter.patch("/:id/status", protect, authorize("seller"), updatePropertyStatus);
+propertyRouter.get("/counts", getPropertyCounts);
+propertyRouter.get("/:id",getPropertyDetails);
+propertyRouter.get("/seller/dashboard", protect, authorize("seller"), getSellerDashboard);
+
+
+export default propertyRouter;
